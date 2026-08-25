9c56753039bf6b015f212a8faa0a6ad07bc2608a
satyamjain1809 <satyamjain1809@gmail.com>
2026-08-25 14:28:43 +0530

    Update 07_apb_maintb.sv

--- VERIFICATION_CODE/07_apb_maintb.sv
+++ VERIFICATION_CODE/07_apb_maintb.sv
@@ -7,55 +7,48 @@ module tb;
    // APB interface instance.
    abp_if vif();
 
-
    // Instantiate APB DUT and connect interface signals.
    apb_ram dut (
-       vif.presetn,
-       vif.pclk,
-       vif.psel,
-       vif.penable,
-       vif.pwrite,
-       vif.paddr,
-       vif.pwdata,
-       vif.prdata,
-       vif.pready,
-       vif.pslverr
+      vif.presetn,
+      vif.pclk,
+      vif.psel,
+      vif.penable,
+      vif.pwrite,
+      vif.paddr,
+      vif.pwdata,
+      vif.prdata,
+      vif.pready,
+      vif.pslverr
    );
-
 
    // Initialize clock.
    initial begin
-     vif.pclk <= 0;
+      vif.pclk <= 0;
    end
-
 
    // Generate clock with 20 time-unit period.
    always #10 vif.pclk <= ~vif.pclk;
 
-
    // Environment handle.
    environment env;
-
 
    // Create environment and start the test.
    initial begin
 
-     env = new(vif);
+      env = new(vif);
 
-     // Generate 30 transactions.
-     env.gen.count = 30;
+      // Generate 30 transactions.
+      env.gen.count = 30;
 
-     env.run();
+      env.run();
 
    end
-
 
    // Generate waveform dump for simulation analysis.
    initial begin
 
-     $dumpfile("dump.vcd");
-     $dumpvars;
+      $dumpfile("dump.vcd");
+      $dumpvars;
 
    end
-
 endmodule
